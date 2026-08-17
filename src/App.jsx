594c3179e63a2b135a5e55f5a3c240bbd0324e55
devjangid28 <devjangid28@users.noreply.github.com>
2026-08-17 18:39:17 +0530

    section 6 done halfy the svg section is pending

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Section2 from './components/Section2.jsx';
 import Section3 from './components/Section3.jsx';
 import Section4 from './components/Section4.jsx';
 import Section5 from './components/Section5.jsx';
+import Section6 from './components/Section6.jsx';
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Section3 />
       <Section4 />
       <Section5 />
+      <Section6 />
     </div>
   );
 }
