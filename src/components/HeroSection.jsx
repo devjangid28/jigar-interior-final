36d9e6b492296e258c87d9fd35bd09e401e5ec09
devjangid28 <devjangid28@users.noreply.github.com>
2026-08-19 12:46:37 +0530

    Revert "colour them updated"
    This reverts commit 7b9a34702c43536381990bbb135c8958902304aa.

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -164,7 +164,7 @@ const HeroSection = () => {
                       <a
                         href={item.href}
                         onClick={item.hasSub ? handleSubOpen : undefined}
-                        style={{ font: '31.9577px "Anek Bangla", sans-serif', color: 'var(--white)' }}
+                        style={{ font: '31.9577px "Anek Bangla", sans-serif', color: '#FFFFFF' }}
                       >
                         {item.label}
                       </a>
